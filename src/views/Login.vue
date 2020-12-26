f552216e74963034e4de30bf751418d4a57cf6f9
Kai Yamada <gpioblink@aol.com>
2020-12-27 06:00:34 +0900

    add google login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -22,7 +22,15 @@
             </v-col>
             <v-col cols="12">
                 <v-btn block large v-on:click="signInWithFacebook">
-                    FaceBookを使用してログイン
+                    Facebookを使用してログイン
+                </v-btn>
+            </v-col>
+            <v-col cols="12">
+                <v-spacer></v-spacer>
+            </v-col>
+            <v-col cols="12">
+                <v-btn block large v-on:click="signInWithFacebook">
+                    Googleを使用してログイン
                 </v-btn>
             </v-col>
         </v-row>
@@ -58,6 +66,28 @@
             },
             signInWithFacebook: function () {
                 const provider = new firebase.auth.FacebookAuthProvider();
+
+                firebase.auth().signInWithPopup(provider).then(function(result) {
+                    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+                    const token = result.credential.accessToken;
+                    // The signed-in user info.
+                    const user = result.user;
+                    // ...
+                    console.log("OK", token, user);
+                }).catch(function(error) {
+                    // Handle Errors here.
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    // The email of the user's account used.
+                    const email = error.email;
+                    // The firebase.auth.AuthCredential type that was used.
+                    const credential = error.credential;
+                    // ...
+                    console.log("NG", errorCode, errorMessage, email, credential);
+                });
+            },
+            signInWithGoogle: function () {
+                const provider = new firebase.auth.GoogleAuthProvider();
 
                 firebase.auth().signInWithPopup(provider).then(function(result) {
                     // This gives you a GitHub Access Token. You can use it to access the GitHub API.
