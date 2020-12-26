267e89c9e6b7852169142fb2e49c3d0565f5f792
roku3 <down01134@gmail.com>
2020-12-26 23:57:08 +0900

    made new

---
+++ src/views/New.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container>
+        <div>
+    <v-text-field
+      label="Main input"
+      :rules="rules"
+      hide-details="auto"
+    ></v-text-field>
+    <v-text-field label="Another input"></v-text-field>
+  </div>
+        <v-row dense>
+            <v-col cols="12">
+                <v-btn height="200px" block large>
+                    旅行に参加
+                </v-btn>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "New.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
