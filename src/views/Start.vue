fc6474a1b8bb25a235fcc828f63c142dda36818d
Kai Yamada <gpioblink@aol.com>
2020-12-26 23:29:27 +0900

    add start panel

---
+++ src/views/Start.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container>
+        <v-row dense>
+            <v-col cols="12">
+                <v-btn height="200px" block large>
+                    新しい旅行帳簿を作成
+                </v-btn>
+            </v-col>
+            <v-col cols="12">
+                <v-spacer></v-spacer>
+            </v-col>
+            <v-col cols="12">
+                <v-btn height="200px" block large>
+                    旅行に参加
+                </v-btn>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Start.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
